dd244b114492869dc3a0bbff1003fcd620818584
n10n <ns68751@gmail.com>
2016-05-10 17:32:28 -0700

    Added node admin support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 EVAL_DIR=/usr/local/splicious/config
+#ENV NODEADMINEMAIL admin@localhost
+#ENV NODEADMINPASS a
+
 #DEPLOYMENT_MODE=colocated
 #MONGODB_HOST=127.0.0.1
 #MONGODB_PORT=27017
@@ -10,6 +13,10 @@ EVAL_DIR=/usr/local/splicious/config
 #BFCLPORT=5672
 #DSLEPSSERVER=127.0.0.1
 #DSLEPSPORT=5672
+
+#Node Admin
+sed -i 's/nodeAdminEmail = \"admin@localhost\"/nodeAdminEmail = \"'$NODEADMINEMAIL'\"/' $EVAL_DIR/eval.conf
+sed -i 's/nodeAdminPass = \"a\"/nodeAdminPass = \"'$NODEADMINPASS'\"/' $EVAL_DIR/eval.conf
 
 #MongdoDB
 sed -i 's/dbHost = \"127.0.0.1\"/dbHost = \"'$MONGODB_HOST'\"/' $EVAL_DIR/eval.conf
