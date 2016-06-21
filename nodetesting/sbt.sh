b09bf2d82ec5d3ce5aead75d57cacb534c29f9a9
n10n <ns68751@gmail.com>
2016-06-21 15:41:57 -0700

    Delete sbt.sh

--- nodetesting/sbt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Taken from: http://www.scala-sbt.org/0.13/tutorial/Manual-Installation.html
-
-SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
