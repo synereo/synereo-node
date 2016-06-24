fb036ccbf04c4c49a667fc9e3cac6eaf80ef2dc4
n10n <ns68751@gmail.com>
2016-06-23 18:10:07 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,6 @@ LIB_DIR=$W_DIR/splicious/lib
 #ENV NODEADMINPASS a
 #DB_HOST=127.0.0.1
 #DB_PORT=27017
-#UBIN = //Update binaries set 1 for update and 0 if not
-#AJAR= agentservices-store-ia-1.9.5.jar//Agent store jar
-#GJAR= gloseval-0.1.jar //Gloseval jar
-#SJAR= specialK-1.1.8.5.jar //SpecialK jar
 
 ## Backend
 #Node Admin
@@ -21,10 +17,14 @@ sed -i 's/nodeAdminPass = \"a\"/nodeAdminPass = \"'$NODEADMINPASS'\"/' $EVAL_FIL
 sed -i 's/dbHost = \"127.0.0.1\"/dbHost = \"'$DB_HOST'\"/' $EVAL_FILE
 sed -i 's/dbPort = \"27017\"/dbPort = \"'$DB_PORT'\"/' $EVAL_FILE
 
+#UBIN = //Update binaries set 1 for update and 0 if not
+#AJAR= agentservices-store-ia-1.9.5.jar//Agent store jar
+#GJAR= gloseval-0.1.jar //Gloseval jar
+#SJAR= specialK-1.1.8.5.jar //SpecialK jar
 if [ "$UBIN" == "1" ]; then
-  wget https://github.com/synereo/compilednode/raw/master/lib/$GJAR -O $LIB_DIR
-  wget https://github.com/synereo/compilednode/raw/master/lib/$AJAR -O $LIB_DIR
-  wget https://github.com/synereo/compilednode/raw/master/lib/$SJAR -O $LIB_DIR
+  wget https://github.com/synereo/compilednode/raw/master/lib/$AJAR -O $LIB_DIR/$AJAR
+  wget https://github.com/synereo/compilednode/raw/master/lib/$GJAR -O $LIB_DIR/$GJAR
+  wget https://github.com/synereo/compilednode/raw/master/lib/$SJAR -O $LIB_DIR/$SJAR
 fi
 
 exec "$@"
