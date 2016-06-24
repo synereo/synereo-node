7b2e9d9333dd6766ff7372c40fd27526f81b49ce
n10n <ns68751@gmail.com>
2016-06-23 22:03:59 -0700

    Update precompiled.sh

--- precompiled.sh
+++ precompiled.sh
@@ -16,9 +16,11 @@ mkdir -p $W_DIR/splicious $S_DIR/libui $S_DIR/lib $S_DIR/logs $S_DIR/config && \
 cd $S_DIR && \
 if [ ! -d $S_DIR/lib ]; then
   svn checkout --force https://github.com/synereo/compilednode/trunk/lib 
+  rm -rf $S_DIR/lib/.svn
 fi
 if [ ! -d $S_DIR/libui ]; then
   svn checkout --force https://github.com/synereo/compilednode/trunk/libui 
+  rm -rf $S_DIR/libui/.svn
 fi
 if [ ! -d $S_DIR/scripts ]; then
   svn checkout --force https://github.com/synereo/gloseval/branches/1.0/scripts 
@@ -26,6 +28,7 @@ if [ ! -d $S_DIR/scripts ]; then
   svn checkout --force https://github.com/synereo/agent-service-ati-ia/branches/1.0/AgentServices-Store/scripts 
   rm -rf $S_DIR/scripts/.svn 
   svn checkout --force https://github.com/synereo/specialk/trunk/scripts 
+  rm -rf $S_DIR/scripts/.svn
 fi 
 
 ############################## Update jar files
@@ -36,16 +39,21 @@ SJAR=specialK-1.1.8.5.jar
 wget https://github.com/synereo/compilednode/raw/master/lib/$AJAR -O $LIB_DIR/$AJAR
 wget https://github.com/synereo/compilednode/raw/master/lib/$GJAR -O $LIB_DIR/$GJAR
 wget https://github.com/synereo/compilednode/raw/master/lib/$SJAR -O $LIB_DIR/$SJAR
+
+LIBUI_DIR=$S_DIR/libui
+UIAJAR=server.server-1.0.1-assets.jar
+UIEJAR=server.server-1.0.1-sans-externalized.jar
+UISJAR=sharedjvm.sharedjvm-0.1-SNAPSHOT.jar
+wget https://github.com/synereo/compilednode/raw/master/libui/$UIAJAR -O $LIBUI_DIR/$UIAJAR
+wget https://github.com/synereo/compilednode/raw/master/libui/$UIEJAR -O $LIBUI_DIR/$UIEJAR
+wget https://github.com/synereo/compilednode/raw/master/libui/$UISJAR -O $LIBUI_DIR/$UISJAR
+
 ##############################
 wget https://github.com/synereo/gloseval/raw/1.0/eval.conf -O $S_DIR/config/eval.conf && \
 wget https://github.com/synereo/gloseval/raw/1.0/log.properties -O $S_DIR/log.properties && \
 wget https://github.com/LivelyGig/ProductWebUI/raw/master/server/src/main/resources/application.conf -O $S_DIR/config/ui.conf && \
 ln -fs config/eval.conf eval.conf && \
 ln -fs config/ui.conf ui.conf && \
-\
-rm -rf $S_DIR/scripts/.svn && \
-rm -rf $S_DIR/lib/.svn && \
-rm -rf $S_DIR/libui/.svn && \
   \
   if [ 0 -eq $DOC ]; then
     if [ ! -d "$S_DIR/agentui" ]; then
